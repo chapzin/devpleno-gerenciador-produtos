49af087ba87ce493c4820b1bb18bcc73b8faf9a3
Ricardo Gomes <ricardogomes@compannydobrasil.com>
2018-12-24 17:53:45 -0300

    add db and product

--- src/components/Categoria/index.js
+++ src/components/Categoria/index.js
@@ -1,5 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
+import api from '../../services/api'
 
-const Categoria = () => <h1>Categoria component</h1>
+class Categoria extends Component {
+  state = {
+    produtos: [],
+    categoria: '',
+    err: ''
+  }
+
+  componentDidMount() {
+    const { catId } = this.props.match.params
+    this.getProdutos(catId)
+  }
+
+  componentWillReceiveProps(newProps) {
+    const { catId } = newProps.match.params
+    this.getProdutos(catId)
+  }
+
+  getProdutos = async id => {
+    try {
+      const { data: prods } = await api.get(`/produtos?catId=${id}`)
+      this.setState({ produtos: prods })
+      const { data: cat } = await api.get(`/categorias/${id}`)
+      this.setState({ categoria: cat })
+    } catch (error) {
+      this.setState({ err: 'Erro ao carregar produtos' })
+      console.warn(error)
+    }
+  }
+
+  renderProdutos(produto) {
+    return (
+      <p className="well" key={produto.id}>
+        {produto.produto}
+      </p>
+    )
+  }
+
+  render() {
+    const { produtos, categoria } = this.state
+    return (
+      <div>
+        <h3>{categoria.categoria}</h3>
+        {produtos.map(this.renderProdutos)}
+      </div>
+    )
+  }
+}
 
 export default Categoria
--- src/pages/Produtos/index.js
+++ src/pages/Produtos/index.js
@@ -2,17 +2,46 @@ import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 import ProdutosHome from '../../components/ProdutosHome'
 import Categoria from '../../components/Categoria'
+import api from '../../services/api'
 
 class Produtos extends Component {
-  render () {
-    const { match } = this.props
+  state = {
+    categorias: [],
+    err: ''
+  }
+
+  componentDidMount() {
+    this.getCategoria()
+  }
+
+  getCategoria = async () => {
+    try {
+      const { data: dados } = await api.get('/categorias')
+      this.setState({ categorias: dados })
+    } catch (error) {
+      this.setState({ err: 'Erro ao carregar as categorias' })
+      console.warn(error)
+    }
+  }
+
+  renderCategoria(cat) {
     return (
-      <div className='row'>
-        <div className='col-md-2'>
+      <li key={cat.id}>
+        <Link to={`/produtos/categoria/${cat.id}`}>{cat.categoria}</Link>
+      </li>
+    )
+  }
+
+  render() {
+    const { match } = this.props
+    const { categorias } = this.state
+    return (
+      <div className="row">
+        <div className="col-md-2">
           <h3>Categorias</h3>
-          <Link to='/produtos/categoria/1'>Categoria 1</Link>
+          <ul>{categorias.map(this.renderCategoria)}</ul>
         </div>
-        <div className='col-md-10'>
+        <div className="col-md-10">
           <h1>Produtos</h1>
           <Route exact path={match.url} component={ProdutosHome} />
           <Route path={match.url + '/categoria/:catId'} component={Categoria} />
